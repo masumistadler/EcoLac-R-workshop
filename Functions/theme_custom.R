8c3003160ae8e66ac24dce09d6feea0e97c07752
Masumi Stadler <39338050+masumistadler@users.noreply.github.com>
2018-10-25 21:25:05 -0400

    Add files via upload

---
+++ Functions/theme_custom.R
@@ -0,0 +1,11 @@
+# Custom theme
+
+theme_custom <- function () { 
+  theme_bw(base_size=12, base_family="Avenir") %+replace% 
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_rect(fill="gray96", colour=NA), 
+      legend.background = element_rect(fill="transparent", colour=NA),
+      legend.key = element_rect(fill="transparent", colour=NA)
+    )
+}
